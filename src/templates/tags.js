373d7133c959b1969c3022fbdce237ad5ab4c038
ayumitk <ayumee528@gmail.com>
2019-07-30 08:29:02 -0700

    update

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { graphql } from 'gatsby';
 import { Link } from 'gatsby-plugin-intl';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage';
