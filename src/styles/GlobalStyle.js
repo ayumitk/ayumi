63278d648245fbe46bbc59d00014fbb5bb024680
ayumitk <ayumee528@gmail.com>
2019-07-30 14:46:56 -0700

    Remove Montserrat font

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -50,7 +50,8 @@ const GlobalStyle = createGlobalStyle`
     font-size: 1.8rem;
     line-height: 1.6;
     color: #606060;
-    font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "nsjp", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    /* font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "nsjp", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"; */
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "nsjp", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
     font-weight: 400;
     background: #f8f8f8;
   }
