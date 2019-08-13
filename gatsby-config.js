fa6b406fe49bc39612e1e809ea13db66c79d2d3c
ayumitk <ayumee528@gmail.com>
2019-08-13 16:20:49 -0700

    Add copy to clipboard button

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,6 +66,14 @@ const cfg = {
         plugins: [
           'gatsby-remark-code-titles', // IMPORTANT: this must be ahead of other plugins that use code blocks
           {
+            resolve: 'gatsby-remark-code-buttons',
+            options: {
+              svgIcon: '<svg viewBox="0 0 561 561"><path d="M395.25 0h-306c-28.05 0-51 22.95-51 51v357h51V51h306V0zm76.5 102h-280.5c-28.05 0-51 22.95-51 51v357c0 28.05 22.95 51 51 51h280.5c28.05 0 51-22.95 51-51V153c0-28.05-22.95-51-51-51zm0 408h-280.5V153h280.5v357z"/></svg>',
+              tooltipText: 'COPY',
+              toasterText: 'Copied to clipboard!',
+            },
+          },
+          {
             resolve: 'gatsby-remark-autolink-headers',
             options: {
               offsetY: '100',
