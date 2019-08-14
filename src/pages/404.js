350bd7e94029366caa9915481b15e7430bbc664d
ayumitk <ayumee528@gmail.com>
2019-08-14 15:03:31 -0700

    Remove lang from 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
@@ -7,17 +6,11 @@ import SEO from '../components/seo';
 import { Container } from '../styles/StyledComponents';
 
 class NotFoundPage extends Component {
-  static propTypes = {
-    intl: PropTypes.object.isRequired,
-  }
-
   render() {
-    const { intl } = this.props;
-
     return (
       <Layout>
         <Container>
-          <SEO title="404: Not Found" lang={intl.locale} />
+          <SEO title="404: Not Found" />
           <h1 style={{ padding: '5rem 0' }}>Not Found</h1>
           <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
         </Container>
